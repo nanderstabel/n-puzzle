65bc681f032ae9d4814871dd79f515fa976614ec
nanderstabel <nanderstabel@gmail.com>
2021-11-24 20:39:04 +0100

    Add input checks

--- src/input.rs
+++ src/input.rs
@@ -7,12 +7,15 @@ use std::{
 };
 use std::num::ParseIntError;
 
-fn parse_input() -> Result<(u8, Vec<Vec<u8>>), ParseIntError> {
+fn get_buffer() -> BufReader<File> {
     let args: Vec<String> = env::args().collect();
     let file = File::open(Path::new(&args[1])).expect("no such file");
-    let buf = BufReader::new(file);
-    let lines = buf.lines();
+    BufReader::new(file)
+}
+
+fn parse_input() -> Result<(u8, Vec<Vec<u8>>), ParseIntError> {
     let mut vector: Vec<Vec<u8>> = Vec::new();
+    let lines = get_buffer().lines();
 
     for line in lines {
         let line = line.unwrap();
@@ -24,10 +27,26 @@ fn parse_input() -> Result<(u8, Vec<Vec<u8>>), ParseIntError> {
             vector.push(result);
         }
     }
+    if vector[0].len() != 1 {
+        "dummy".parse::<u8>()?;
+    }
     Ok((vector[0][0], vector[1..].to_vec()))
 }
 
 pub fn get_grid() -> Result<Grid, ParseIntError> {
     let (size, grid) = parse_input()?;
+    for row in &grid {
+        if row.len() != size.into() {
+            "dummy".parse::<u8>()?;
+        }
+    }
+    let flat: Vec<&u8> = grid.iter().flat_map(|v| v).collect();
+    let set: Vec<u8> = (0..(size * size)).collect();
+    if set.iter().all(|u| flat.contains(&u)) == false {
+        "dummy".parse::<u8>()?;
+    }
+    if flat.len() != set.len() {
+        "dummy".parse::<u8>()?;
+    }
     Ok(grid)
 }
