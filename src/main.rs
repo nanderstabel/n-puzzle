57b8c1c6f2fee6de3e267e45dc917b94a8a8af11
nanderstabel <nanderstabel@gmail.com>
2021-11-24 16:05:09 +0100

    Add basic error handling

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/22 15:48:20 by nstabel       #+#    #+#                 */
-/*   Updated: 2021/11/23 23:06:36 by nstabel       ########   odam.nl         */
+/*   Updated: 2021/11/24 15:12:14 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,39 @@ use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
+    error::Error,
     collections::HashMap
 };
 use std::num::ParseIntError;
 
-fn get_puzzle(filename: impl AsRef<Path>) -> Result<Grid, ()> {
-    let file = File::open(filename).expect("no such file");
+fn get_puzzle(file: File) -> Result<Grid, ParseIntError> {
     let buf = BufReader::new(file);
     let mut grid = Grid::new();
 
     let lines = buf.lines();
 
     for line in lines {
-        let temp = line.unwrap();
-        let temp0: &str = temp.split('#').collect::<Vec<_>>()[0];
-        if temp0.starts_with("#") {
+        let line = line.unwrap();
+        let string: &str = line.split('#').collect::<Vec<_>>()[0];
+        if string.starts_with("#") {
             continue;
         }
-        let temp2: Vec<_> = temp0.split_whitespace().collect();
-        let temp3: Result<Vec<u8>, _> = temp2.into_iter()
-                                                .map(|x| x.parse::<u8>())
-                                                .collect();
-        grid.push(temp3.unwrap());
+        let result: Vec<u8> = string.split_whitespace()
+                                    .map(|x| x.parse::<u8>())
+                                    .collect::<Result<Vec<u8>, _>>()?;
+        grid.push(result);
     }
     Ok(grid)
 }
 
-fn main () {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
+    let file = File::open(Path::new(&args[1])).expect("no such file");
     // let puzzle = Puzzle::default();
     // let mut test = HashMap::new();
 
 
-    let grid = get_puzzle(&args[1]).unwrap();
+    let grid = get_puzzle(file)?;
     println!("{:#?}", grid);
-
+    Ok(())
 }
