a818072671bf75c72cd9e5c2bc90c920eb40ea2c
nanderstabel <nanderstabel@gmail.com>
2021-11-24 22:39:42 +0100

    Add Data struct and data hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,42 @@ use std::fmt;
 
 pub type Row = Vec<u8>;
 pub type Grid = Vec<Row>;
+pub type Location = (usize, usize);
+
+#[derive(Debug, Clone)]
+pub struct Data {
+    pub h: u16,
+    pub location: Location
+}
+
+impl Data {
+    pub fn new(h: u16, location: Location) -> Self {
+        Data { h: h, location: location }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct Node<'a> {
     pub grid: Grid,
+    pub cursor: Location,
+    pub h: u16,
     pub parent: Option<&'a Node<'a>>
 }
 
 impl fmt::Display for Node<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for row in &self.grid {
-            write!(f, "{:2?}\n", row)?;
+            for u in row {
+                match u {
+                    0 => write!(f, "    ")?,
+                    _ => write!(f, "{:4}", u)?
+                }
+            }
+            write!(f, "\n")?;
         }
         Ok(())
     }
 }
-
 
 #[derive(Debug, Default)]
 pub struct Puzzle {
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/22 15:48:20 by nstabel       #+#    #+#                 */
-/*   Updated: 2021/11/24 21:18:08 by nstabel       ########   odam.nl         */
+/*   Updated: 2021/11/24 22:37:21 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,19 @@ use n_puzzle::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // let puzzle = Puzzle::default();
-    // let mut test = HashMap::new();
-
-    let start = Node {
+    let mut data = HashMap::new();
+    let mut start = Node {
         grid: get_grid()?,
+        cursor: (0, 0),
+        h: 0,
         parent: None
     };
+    for (y, row) in start.grid.iter().enumerate() {
+        for (x, u) in row.iter().enumerate() {
+            data.insert(u, Data::new(0, (y, x)));
+        }
+    }
     println!("{}", start);
+    println!("{:#?}", data);
     Ok(())
 }
