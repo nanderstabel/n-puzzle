035469a0fdc7a1e70109a338ca9194d9207f7a69
nanderstabel <nanderstabel@gmail.com>
2021-11-28 05:18:22 +0100

    Move heuristics to separate file

---
+++ src/heuristics.rs
@@ -0,0 +1,11 @@
+pub fn manhattan((cy, cx): (usize, usize), (ey, ex): (usize, usize)) -> u16 {
+    (if cy > ey { cy - ey } else { ey - cy } + if cx > ex { cx - ex } else { ex - cx }) as u16
+}
+
+pub fn hamming((cy, cx): (usize, usize), (ey, ex): (usize, usize)) -> u16 {
+    if (cy, cx) == (ey, ex) {
+        0
+    } else {
+        1
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
+mod heuristics;
 mod input;
 mod structs;
 
+use crate::heuristics::*;
 use std::error::Error;
 use structs::*;
-
-fn manhattan((cy, cx): (usize, usize), (ey, ex): (usize, usize)) -> u16 {
-    (if cy > ey { cy - ey } else { ey - cy } + if cx > ex { cx - ex } else { ex - cx }) as u16
-}
-
-fn hamming((cy, cx): (usize, usize), (ey, ex): (usize, usize)) -> u16 {
-    if (cy, cx) == (ey, ex) {
-        0
-    } else {
-        1
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut puzzle = puzzle::Puzzle::new(manhattan);
